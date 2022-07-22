8ed5ba7887210a7819998be15dfc4ce324008033
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-07-22 14:44:10 -0400

    Finalized backup script

--- backup.py
+++ backup.py
@@ -35,10 +35,13 @@ def commit_csv() -> None:
     """
     with tempfile.TemporaryDirectory() as dir:
         repo = Repo.clone_from(
-            "https://github.com/jrg94/personal-data.git", dir)
+            "https://github.com/jrg94/personal-data.git", dir
+        )
         health_data_path = Path(dir) / "health"
-        shutil.copyfile("data/fitbit.csv",
-                        str(health_data_path / "fitbit.csv"))
+        shutil.copyfile(
+            "fitbit.csv",
+            str(health_data_path / "fitbit.csv")
+        )
         repo.index.add([str(health_data_path / "fitbit.csv")])
         commit = repo.index.commit(f"Updated fitbit data automatically")
         if not commit.stats.files:
@@ -47,45 +50,46 @@ def commit_csv() -> None:
             log.info(f"Committing changes: {commit.stats.files}")
             repo.remote(name="origin").push()
         repo.close()
-        
-        
+
+
 def get_row_of_data(date: str) -> tuple[pd.DataFrame, int]:
     """
     Grabs a day's worth of data from the Fitbit API.
-    
+
     :param date: the date to pull data for
     :return: a dataframe of the data and the number of requests to generate a row
     """
     to_df = {}
     columns = {
-        "dateTime": "Date", 
-        "value": "Steps", 
-        "bmi": "BMI", 
-        "fat": "Body Fat %", 
-        "weight": "Weight", 
+        "dateTime": "Date",
+        "value": "Steps",
+        "bmi": "BMI",
+        "fat": "Body Fat %",
+        "weight": "Weight",
         "totalMinutesAsleep": "Total Sleep (minutes)",
         "totalSleepRecords": "Total Sleep Records",
         "totalTimeInBed": "Total Time in Bed (minutes)",
     }
-    
+
     day_of_sleep: dict = client.sleep(date)
     log.info(f"Retrieve sleep data for {date}: {day_of_sleep}")
     day_of_sleep = day_of_sleep["summary"]
     day_of_sleep.pop("stages", None)
     if all(v > 0 for v in day_of_sleep.values()):
         to_df |= day_of_sleep
-    
-    day_of_steps: dict = client.time_series("activities/steps", base_date=date, period="1d")
+
+    day_of_steps: dict = client.time_series(
+        "activities/steps", base_date=date, period="1d")
     log.info(f"Retrieve steps data for {date}: {day_of_steps}")
     day_of_steps = day_of_steps["activities-steps"][0]
     to_df |= day_of_steps
-    
+
     day_of_body: dict = client.body(date)
     log.info(f"Retrieve body data for {date}: {day_of_body}")
     day_of_body = day_of_body["body"]
     if all(v > 0 for v in day_of_body.values()):
         to_df |= day_of_body
-    
+
     df = pd.DataFrame([to_df])
     df.rename(columns=columns, inplace=True)
     df["Date"] = pd.to_datetime(df["Date"])
@@ -97,18 +101,19 @@ def get_row_of_data(date: str) -> tuple[pd.DataFrame, int]:
 def get_latest_data():
     """
     Takes the existing data and updates it with the latest data.
-    
+
     :return: a dataframe of the complete data set
     """
     requests = 0
-    df = pd.read_csv("https://raw.githubusercontent.com/jrg94/personal-data/main/health/fitbit.csv")
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/jrg94/personal-data/main/health/fitbit.csv")
     df["Date"] = pd.to_datetime(df["Date"])
     df.set_index("Date", inplace=True)
     latest_date = df.index.max()
     date_range = pd.date_range(
-        start=latest_date, 
-        end=datetime.today(), 
-        freq="D" 
+        start=latest_date,
+        end=datetime.today(),
+        freq="D"
     )
     try:
         for date in date_range:
@@ -157,10 +162,12 @@ if __name__ == "__main__":
         refresh_cb=refresh_cb
     )
 
+    # Collect data
     latest_df, requests = get_latest_data()
     log.info(f"Made {requests} requests while updating the data.")
-    
-    latest_df.to_csv("data/fitbit.csv")
-    
-    log.info(f"Committing the data to the personal data repo:\n{latest_df}")
+    log.info(f"Finalized the latest data collection:\n{latest_df}")
+
+    # Backup data
+    latest_df.to_csv("fitbit.csv")
     commit_csv()
+    os.remove("fitbit.csv")
